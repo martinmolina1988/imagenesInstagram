58786fe73e2008281f5b2e9faf1397eec113e298
martinmolina1988 <67392381+martinmolina1988@users.noreply.github.com>
2022-04-01 04:52:25 -0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import "./App.css";
-import { useState } from "react";
-import ImageComponent from "./ImageComponent";
+import React, { useState } from "react";
+import html2canvas from "html2canvas";
 
 function App() {
   const [url, setUrl] = useState("");
   const onChange = (e) => {
     setUrl(e.target.value);
   };
+  const printRef = React.useRef();
   console.log(url);
+
+  const handleDownloadImage = async () => {
+    const element = printRef.current;
+    const canvas = await html2canvas(element,{useCORS: true, scale: 1});
+
+    const data = canvas.toDataURL("image/jpg");
+    const link = document.createElement("a");
+
+    if (typeof link.download === "string") {
+      link.href = data;
+      link.download = "image.jpg";
+
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    } else {
+      window.open(data);
+    }
+  };
   return (
     <div className="App">
       <h2>Subir archivo</h2>
       <label htmlFor="">imagen</label>
       <input type="text" onChange={(e) => onChange(e)} />
       <br />
-      <ImageComponent>
+      <div>
         <div
+          ref={printRef}
           className="contenedor-imagen"
           style={{ backgroundImage: `url(${url})` }}
         />
-      </ImageComponent>
+
+        <h2 style={{ position: "absolute", right: 0 }}>hola</h2>
+      </div>
+      <button type="button" onClick={handleDownloadImage}>
+        Download as Image
+      </button>
     </div>
   );
 }
